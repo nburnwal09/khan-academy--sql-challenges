d29a4acfbb6b88391f9cd372f1935744e38697b1
Neha Burnwal <47716641+nburnwal09@users.noreply.github.com>
2020-11-15 23:32:40 +0530

    Add files via upload

---
+++ Booklist.sql
@@ -0,0 +1,6 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+INSERT INTO books VALUES(1, "famous five", 4);
+INSERT INTO books VALUES(2, "twilight",5);
+INSERT INTO books VALUES(3, "seven secrets", 4);
+
+SELECT * FROM books;
